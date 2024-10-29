08d445deaa3d190958dde40bd6df094f983529e9
scottreynolds6294@gmail.com <scottreynolds6294@gmail.com>
2024-10-28 20:41:00 -0400

    typeDefs and queries on client side implemented

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const typeDefs = `
+    type User {
+        _id: ID
+        username: String
+        email: String   
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String    
+    }
+
+    Auth {
+        token: ID!
+        user: User
+    }
+
+    input BookInput {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
