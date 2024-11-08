d270440a1daa67f13decae6147e4dc59d2b51b41
scottreynolds6294@gmail.com <scottreynolds6294@gmail.com>
2024-11-08 15:09:23 -0500

    connections

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect( 'mongodb+srv://scottreynolds6294:<terps4114>@cluster0.c4tnf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    ssl: true,
 });
 
 module.exports = mongoose.connection;
